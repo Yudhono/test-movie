29495effa487760ee50f0c1d6f7ce23882bf32c3
yudhonowitanto.yw <yudhonowitanto.yw@gmail.com>
2022-05-18 15:59:58 +0700

    fix fungsi add watch list

--- src/components/ContentModal/ContentModal.js
+++ src/components/ContentModal/ContentModal.js
@@ -1,69 +1,64 @@
-import * as React from 'react';
-import { useEffect, useState, useContext } from 'react';
-import axios from 'axios';
-import Backdrop from '@mui/material/Backdrop';
-import Fade from '@mui/material/Fade';
-import Button from '@mui/material/Button';
-import Modal from '@mui/material/Modal';
+import * as React from "react";
+import { useEffect, useState, useContext } from "react";
+import axios from "axios";
+import Backdrop from "@mui/material/Backdrop";
+import Fade from "@mui/material/Fade";
+import Button from "@mui/material/Button";
+import Modal from "@mui/material/Modal";
 import {
-    img_500,
-    unavailable,
-    unavailableLandscape,
+  img_500,
+  unavailable,
+  unavailableLandscape,
 } from "../../config/config";
-import styles from './ContentModal.module.css';
-import Box from '@mui/material/Box';
+import styles from "./ContentModal.module.css";
+import Box from "@mui/material/Box";
 
 import { GlobalContext } from "../../context/GlobalState";
 
 export default function TransitionsModal({ children, media_type, id, movie }) {
+  const { addMovieToWatchlist, watchlist } = useContext(GlobalContext);
 
-const {
-    addMovieToWatchlist,
-    watchlist,
-} = useContext(GlobalContext);
+  console.log(777, "watchlist", watchlist);
 
-let storedMovie = watchlist.find((o) => o.id === movie.id);
-console.log('halo:',storedMovie);
-console.log('movie',movie)
+  let storedMovie = watchlist.find((o) => o.id === movie.id);
+  console.log("halo:", storedMovie);
+  console.log(777, "movie", movie);
 
-const watchlistDisabled = storedMovie
-? true
-: false;
+  const watchlistDisabled = storedMovie ? true : false;
 
+  const [open, setOpen] = useState(false);
+  const [content, setContent] = useState();
 
-const [open, setOpen] = useState(false);
-const [content, setContent] = useState();
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
 
-const handleOpen = () => setOpen(true);
-const handleClose = () => setOpen(false);
-
-const fetchData = async () => {
+  const fetchData = async () => {
     const { data } = await axios.get(
-        `https://api.themoviedb.org/3/${media_type}/${id}?api_key=${process.env.NEXT_PUBLIC_API_KEY}&language=en-US`
+      `https://api.themoviedb.org/3/${media_type}/${id}?api_key=${process.env.NEXT_PUBLIC_API_KEY}&language=en-US`
     );
 
     setContent(data);
     // console.log(data);
-};
+  };
 
-console.log(addMovieToWatchlist);
+  console.log(addMovieToWatchlist);
 
-useEffect(() => {
+  useEffect(() => {
     fetchData();
     // eslint-disable-next-line
-}, []);
+  }, []);
 
-return (
+  return (
     <>
-    <div
+      <div
         className={styles.media}
         style={{ cursor: "pointer" }}
         color="inherit"
         onClick={handleOpen}
-    >
+      >
         {children}
-    </div>
-    <Modal
+      </div>
+      <Modal
         aria-labelledby="transition-modal-title"
         aria-describedby="transition-modal-description"
         open={open}
@@ -71,18 +66,18 @@ return (
         closeAfterTransition
         BackdropComponent={Backdrop}
         BackdropProps={{
-            timeout: 500,
+          timeout: 500,
         }}
         sx={{
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
         }}
-    >
-
-    <Fade in={open}>
-        {content && (
-            <Box sx={{
+      >
+        <Fade in={open}>
+          {content && (
+            <Box
+              sx={{
                 width: "60%",
                 height: "70%",
                 backgroundColor: "#030b17",
@@ -91,59 +86,56 @@ return (
                 color: "white",
                 boxShadow: 0,
                 padding: 1,
-            }}>
-
-                <div className={styles.ContentModal}>
+              }}
+            >
+              <div className={styles.ContentModal}>
                 <img
-                    src={
+                  src={
                     content.poster_path
-                        ? `${img_500}/${content.poster_path}`
-                        : unavailable
-                    }
-                    alt={content.name || content.title}
-                    className={styles.ContentModal__portrait}
+                      ? `${img_500}/${content.poster_path}`
+                      : unavailable
+                  }
+                  alt={content.name || content.title}
+                  className={styles.ContentModal__portrait}
                 />
                 <img
-                    src={
+                  src={
                     content.backdrop_path
-                        ? `${img_500}/${content.backdrop_path}`
-                        : unavailableLandscape
-                    }
-                    alt={content.name || content.title}
-                    className={styles.ContentModal__landscape}
+                      ? `${img_500}/${content.backdrop_path}`
+                      : unavailableLandscape
+                  }
+                  alt={content.name || content.title}
+                  className={styles.ContentModal__landscape}
                 />
                 <div className={styles.ContentModal__about}>
-                    <span className={styles.ContentModal__title}>
-                        {content.name || content.title} - {''}
-                            {(
-                                content.first_air_date ||
-                                content.release_date ||
-                                "-----"
-                            ).substring(0, 4)}
-                    </span>
-                    
-                    <span className={styles.ContentModal__description}>
-                        {content.overview}
-                    </span>
+                  <span className={styles.ContentModal__title}>
+                    {content.name || content.title} - {""}
+                    {(
+                      content.first_air_date ||
+                      content.release_date ||
+                      "-----"
+                    ).substring(0, 4)}
+                  </span>
 
-                    <Button
-                        variant="contained"
-                        color="primary"
-                        target="__blank"
-                        disabled={watchlistDisabled}
-                        onClick={() => addMovieToWatchlist(movie)}
-                        >
-                        ADD TO FAVORITE
-                    </Button>
+                  <span className={styles.ContentModal__description}>
+                    {content.overview}
+                  </span>
+
+                  <Button
+                    variant="contained"
+                    color="primary"
+                    target="__blank"
+                    disabled={watchlistDisabled}
+                    onClick={() => addMovieToWatchlist(movie)}
+                  >
+                    ADD TO FAVORITE
+                  </Button>
                 </div>
-            </div>
+              </div>
             </Box>
-         
-    )}
-    </Fade>
-
-    </Modal>
+          )}
+        </Fade>
+      </Modal>
     </>
-);
+  );
 }
-  
--- src/components/SingleContent/SingleContent.js
+++ src/components/SingleContent/SingleContent.js
@@ -1,46 +1,48 @@
-import Badge from '@mui/material/Badge';
+import Badge from "@mui/material/Badge";
 import { img_300, unavailable } from "../../config/config";
 import ContentModal from "../ContentModal/ContentModal";
-import styles from './SingleContent.module.css';
+import styles from "./SingleContent.module.css";
 
-import Paper from '@mui/material/Paper';
-import Box from '@mui/material/Box';
-import Image from 'next/image'
+import Paper from "@mui/material/Paper";
+import Box from "@mui/material/Box";
+import Image from "next/image";
 import { MovieControls } from "../Favorite/MovieControls";
 
 const SingleContent = ({
-    id,
-    poster,
-    title,
-    date,
-    media_type,
-    vote_average,
-    type,movie,
+  id,
+  poster,
+  title,
+  date,
+  media_type,
+  vote_average,
+  type,
+  movie,
 }) => {
-
-return (
-    <ContentModal media_type={media_type} id={id}>
-        <Box sx={{
-            width: 300,
-            height: 550,
-            paddingTop: 3,
-        }}>
-            <img
-                className={styles.poster}
-                src={poster ? `${img_300}${poster}` : unavailable}
-                alt={title}
-            />
-            <Badge
-                badgeContent={vote_average}
-                color={vote_average > 6 ? "primary" : "secondary"}
-            />
-            <b className={styles.title}>{title}</b>
-            <span className={styles.subTitle}>
-                {media_type === "tv" ? "TV Series" : "Movie"}
-                <span className={styles.subTitle}>{date}</span>
-            </span>
-        </Box>
-        <MovieControls type={type} movie={movie} />
+  return (
+    <ContentModal media_type={media_type} id={id} movie={movie}>
+      <Box
+        sx={{
+          width: 300,
+          height: 550,
+          paddingTop: 3,
+        }}
+      >
+        <img
+          className={styles.poster}
+          src={poster ? `${img_300}${poster}` : unavailable}
+          alt={title}
+        />
+        <Badge
+          badgeContent={vote_average}
+          color={vote_average > 6 ? "primary" : "secondary"}
+        />
+        <b className={styles.title}>{title}</b>
+        <span className={styles.subTitle}>
+          {media_type === "tv" ? "TV Series" : "Movie"}
+          <span className={styles.subTitle}>{date}</span>
+        </span>
+      </Box>
+      <MovieControls type={type} movie={movie} />
     </ContentModal>
   );
 };
